e2726a1b3aaf27e68efb27e3017ee374969b450c
Nickolas Thompson <thompsonnickolas1@gmail.com>
2022-05-31 15:07:49 -0400

    internet troubles

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var date = moment().format("MMM D, YYYY");
 var time = moment().format("LTS");
+var hour = moment().format("h:m");
 
 console.log(date, time)
 
@@ -10,35 +11,44 @@ var saveButton = $(".saveBtn");
 function handleSaveButton() {
     localStorage.setItem("To Do: ", toDo.val());
 
-    
+
     console.log("To Do: ", toDo.val());
     console.log(date, time)
 }
 
 function displayTime() {
-    $(".hour").text(moment().format(MMM ,D ,YYYY, LTS));
+    $(".hour").text(moment().format(MMM, D, YYYY, LTS));
 
-    for (let i = 9; i <=18; i++) {
+    for (let i = 9; i <= 18; i++) {
         var timeEvent = saveTime[i] || "";
         $(".container").append(createTimeBlock(i, timeEvent));
 
     }
-    
-    
 
+}
+
+function addingCSS() {
+    for (let i = 9; i <= 18; i++) {
+        if (hour) {
+            $("textarea").parent().addClass("present");
+        } else if (i < hour) {
+            $("textarea").parent().addClass("past");
+        } else if (i > hour) {
+            $("textarea").parent().addClass("future");
+        }
+    }
 }
 
 
 // creating the click event for the save button
-saveButton.on("click", function(event) {
-    handleSaveButton();
-    event.preventDefault();
-    
-    // var saveTime = JSON.parse(localStorage.getItem("scheduleTime")) || {};
-    // var target = $(event.target);
-    // var hour = target.siblings(".hour").text();
-    // var textArea = target.siblings(".toDo").val();
+saveButton.on("click", function (event) {
+    var whoAmI = $(event.target).parent().parent().attr("id");
+    var whatTimeIsIt = whoAmI.substring(4)
+    var whatIsTheParent = $("#time" + whatTimeIsIt)
 
-    // saveTime[hour] = textArea;
-    // localStorage.setItem("scheduleTime", JSON.stringify(saveTime));
+    localStorage.setItem("time" + whatTimeIsIt, whatIsTheParent.children().eq(1).children().eq(0).val().trim());
+
+    addingCSS();
+
+
 });
